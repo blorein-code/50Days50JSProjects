d8319a714013819104168494b2e7071429900006
Berke <bblorein@gmail.com>
2022-02-21 00:47:45 +0300

    last version

--- RotatingNav/script.js
+++ RotatingNav/script.js
@@ -1,14 +1,11 @@
-// const open = document.getElementById("open")
-// const close = document.getElementById("close")
-// const container = document.querySelector(".container")
-
-var open = $('#open')
-var close = $('#close')
-var container = $('.container')
-
-$("#open").on("click", () => container.addClass('show-nav'));
-$("#close").on("click", () => container.removeClass('show-nav'));
+const open = document.getElementById("open")
+const close = document.getElementById("close")
+const container = document.querySelector(".container")
 
 
-// open.addEventListener('click', ()=>container.classList.add('show-nav'))
-// close.addEventListener('click', ()=>container.classList.remove('show-nav'))
+// $("open").("click", () => container.addClass('show-nav'));
+// $("close").("click", () => container.removeClass('show-nav'));
+
+
+open.addEventListener('click', ()=>container.classList.add('show-nav'))
+close.addEventListener('click', ()=>container.classList.remove('show-nav'))
