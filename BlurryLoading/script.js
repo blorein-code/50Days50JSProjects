0be58e12c5aea3c65e09814f028178003cc37fb4
Berke <85216724+blorein-code@users.noreply.github.com>
2022-02-24 10:49:00 +0300

    jquery version done.

--- BlurryLoading/script.js
+++ BlurryLoading/script.js
@@ -11,12 +11,15 @@ function blurring () {
     load++
     if (load > 99) {
     clearInterval(int)
-    console.log(load);
+    // console.log(load);
     }
 
     loadText.innerText = `${load}%`;
     loadText.style.opacity = scale(load,0,100,1,0);
     bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`;
+    // loadText.text(`${load}%`)
+    // loadText.css("opacity", scale(load,0,100,1,0));
+    // bg.css("filter",`blur(${scale(load,0,100,30,0)}px)`)
 }
 
 //https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
---
+++ BlurryLoading/scriptwithjquery.js
@@ -0,0 +1,30 @@
+const loadText =  $('.loading-text');
+const bg =  $('.bg');
+// const loadText = document.querySelector('.loading-text')
+// const bg = document.querySelector('.bg')
+
+
+let load = 0;
+let int = setInterval(blurring,30);
+
+function blurring () {
+    load++
+    if (load > 99) {
+    clearInterval(int)
+    console.log(load);
+    }
+
+    // loadText.innerText = `${load}%`;
+    // loadText.style.opacity = scale(load,0,100,1,0);
+    // bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`;
+    loadText.text(`${load}%`)
+    loadText.css("opacity", scale(load,0,100,1,0));
+    bg.css("filter",`blur(${scale(load,0,100,30,0)}px)`)
+}
+
+//https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
