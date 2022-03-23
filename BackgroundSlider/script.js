454a98b3c5e6feadb8070bf730c2acda5b8ac232
Berke <bblorein@gmail.com>
2022-03-23 17:47:22 +0300

    will continue when I arrived to home :D

---
+++ BackgroundSlider/script.js
@@ -0,0 +1,10 @@
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+
+const leftBtn = document.getElementById('right')
+
+let activeSlide = 0
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[]
+}
